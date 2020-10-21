5cb280a39ffcae478602905ee6cc05c81ceaec7a
Tendai Nyandoro <tnyandoro@gmail.com>
2020-10-21 11:41:57 +0200

    Add more testing methods correct my_each

--- enumerable.rb
+++ enumerable.rb
@@ -8,23 +8,22 @@ module Enumerable
       yield(arr[i])
       i += 1
     end
-    arr
+    self
   end
 
   def my_each_with_index
-    return to_enum(my_each_with_index) unless block_given?
+    return to_enum(:my_each_with_index) unless block_given?
 
     i = 0
-    arr = to_a
-    while i < arr.length
-      yield(arr[i], i)
+    while i < to_a.length
+      yield to_a([i], i)
       i += 1
     end
     self
   end
 
   def my_select
-    return to_enum(my_select) unless block_given?
+    return to_enum(:my_select) unless block_given?
 
     new_arr = []
     to_a.my_each { |item| new_arr << item if yield(item) }
@@ -170,3 +169,7 @@ end
 def multiply_els(arr)
   arr.my_inject(:*)
 end
+
+p [1, 2, 3].my_each(&proc { |x| x > 2 }) == [1, 2, 3]
+p [1, 2, 3].my_each_with_index.class == Enumerator
+p [1,2,3,4].my_select.class == Enumerator
