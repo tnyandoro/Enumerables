5de829f9cb3cd50674721d5308d509599bb11839
Tendai Nyandoro <tnyandoro@gmail.com>
2020-10-23 09:57:04 +0200

    Add the return statement for the array and index

--- enumerable.rb
+++ enumerable.rb
@@ -1,25 +1,29 @@
 module Enumerable
   def my_each
-    return to_enum(:my_each) unless block_given?
+    return to_enum unless block_given?
 
-    i = 0
+    pos = 0
     arr = to_a
-    while i < arr.length
-      yield(arr[i])
-      i += 1
+    while pos < arr.length
+      yield(arr[pos])
+      pos += 1
     end
     self
   end
 
   def my_each_with_index
-    return to_enum(:my_each_with_index) unless block_given?
+    return to_enum unless block_given?
 
-    i = 0
-    while i < to_a.length
-      yield to_a([i], i)
-      i += 1
+    arr = to_a
+    pos = 0
+    my_arr = []
+    while pos < arr.length
+      yield(arr[pos], pos)
+      my_arr << arr[pos]
+      my_arr << pos
+      pos += 1
     end
-    self
+    my_arr
   end
 
   def my_select
@@ -183,32 +187,3 @@ def multiply_els(arr)
   arr.my_inject(:*)
 end
 
-p (0...7).my_each
-n = [2, 2, 6, 6]
-hs = {"ruby" => "programming", "OOP" => "develop"}
-puts n.all?(&:even?)
-puts n.my_all?(&:even?)
-puts n.my_any?(&:even?)
-puts n.my_none?(&:even?)
-puts n.my_count(&:even?)
-puts n.my_map(&:even?)
-
-p (5..10).my_inject { |sum, n| sum + n }
-p (5..10).my_inject { |product, n| product * n }
-p (5..10).my_inject(:+)
-puts multiply_els(n)
-
-p [1, 2, 3, 4].my_inject
-p [1, 2, 3].my_each(&proc { |x| x > 2 }) == [1, 2, 3]
-p [1, 2, 3].my_each_with_index.class == Enumerator
-p [1, 2, 3, 4].my_select.class == Enumerator
-p [true, [false]].all? == [true, [false]].my_all?
- my_proc = Proc.new { |n| (n % 7).zero? }
-
-p [1,2,3].count(&proc{|num|num%2==0}) == [1,2,3].my_count(&proc{|num|num%2==0})
-p [1, 2, 3, 4, 5].my_none?(&my_proc) == [1, 2, 3, 4, 5].none?(&my_proc)
-p [false, nil, false].none? == [false, nil, false].my_none?
-p %w[dog cat].none?(/x/) == %w[dog cat].my_none?(/x/)
-p ['dog','car'].none?(5) == ['dog','car'].my_none?(5)
-p [1, 2, 3].count(&my_proc)
-p [1, 2, 3].my_none?(1)
--- test.rb
+++ test.rb
@@ -1,31 +1,66 @@
-require_relative 'enmerable.rb'
+# require_relative 'enmerable.rb'
 
-p (0...7).my_each
-n = [2, 2, 6, 6]
-hs = {"ruby" => "programming", "OOP" => "develop"}
-puts n.all?(&:even?)
-puts n.my_all?(&:even?)
-puts n.my_any?(&:even?)
-puts n.my_none?(&:even?)
-puts n.my_count(&:even?)
-puts n.my_map(&:even?)
+# p (0...7).my_each
+# n = [2, 2, 6, 6]
+# hs = { 'ruby' => 'programming', 'OOP' => 'develop' }
+# puts n.all?(&:even?)
+# puts n.my_all?(&:even?)
+# puts n.my_any?(&:even?)
+# puts n.my_none?(&:even?)
+# puts n.my_count(&:even?)
+# puts n.my_map(&:even?)
 
-p (5..10).my_inject { |sum, n| sum + n }
-p (5..10).my_inject { |product, n| product * n }
-p (5..10).my_inject(:+)
-puts multiply_els(n)
+# p(5..10).my_inject { |sum, n| sum + n }
+# p(5..10).my_inject { |product, n| product * n }
+# p (5..10).my_inject(:+)
+# puts multiply_els(n)
 
-p [1, 2, 3, 4].my_inject
-p [1, 2, 3].my_each(&proc { |x| x > 2 }) == [1, 2, 3]
-p [1, 2, 3].my_each_with_index.class == Enumerator
-p [1, 2, 3, 4].my_select.class == Enumerator
-p [true, [false]].all? == [true, [false]].my_all?
- my_proc = Proc.new { |n| (n % 7).zero? }
+# p [1, 2, 3, 4].my_inject
+# p [1, 2, 3].my_each(&proc { |x| x > 2 }) == [1, 2, 3]
+# p [1, 2, 3].my_each_with_index.class == Enumerator
+# p [1, 2, 3, 4].my_select.class == Enumerator
+# p [true, [false]].all? == [true, [false]].my_all?
+# my_proc = proc { |n| (n % 7).zero? }
 
-p [1,2,3].count(&proc{|num|num%2==0}) == [1,2,3].my_count(&proc{|num|num%2==0})
-p [1, 2, 3, 4, 5].my_none?(&my_proc) == [1, 2, 3, 4, 5].none?(&my_proc)
-p [false, nil, false].none? == [false, nil, false].my_none?
-p %w[dog cat].none?(/x/) == %w[dog cat].my_none?(/x/)
-p ['dog','car'].none?(5) == ['dog','car'].my_none?(5)
-p [1, 2, 3].count(&my_proc)
-p [1, 2, 3].my_none?(1)
+# p [1, 2, 3].count(&proc { |num| num.even? }) == [1, 2, 3].my_count(&proc { |num| num.even? })
+# p [1, 2, 3, 4, 5].my_none?(&my_proc) == [1, 2, 3, 4, 5].none?(&my_proc)
+# p [false, nil, false].none? == [false, nil, false].my_none?
+# p %w[dog cat].none?(/x/) == %w[dog cat].my_none?(/x/)
+# p %w[dog car].none?(5) == %w[dog car].my_none?(5)
+# p [1, 2, 3].count(&my_proc)
+# p [1, 2, 3].my_none?(1)
+
+# p (0...7).my_each
+# n = [2, 2, 6, 6]
+# hs = {"ruby" => "programming", "OOP" => "develop"}
+# puts n.all?(&:even?)
+# puts n.my_all?(&:even?)
+# puts n.my_any?(&:even?)
+# puts n.my_none?(&:even?)
+# puts n.my_count(&:even?)
+# puts n.my_map(&:even?)
+
+# p (5..10).my_inject { |sum, n| sum + n }
+# p (5..10).my_inject { |product, n| product * n }
+# p (5..10).my_inject(:+)
+# puts multiply_els(n)
+
+# p [1, 2, 3, 4].my_inject
+# p [1, 2, 3].my_each(&proc { |x| x > 2 }) == [1, 2, 3]
+# p [1, 2, 3].my_each_with_index.class == Enumerator
+# p [1, 2, 3, 4].my_select.class == Enumerator
+# p [true, [false]].all? == [true, [false]].my_all?
+#  my_proc = Proc.new { |n| (n % 7).zero? }
+
+# p [1,2,3].count(&proc{|num|num%2==0}) == [1,2,3].my_count(&proc{|num|num%2==0})
+# p [1, 2, 3, 4, 5].my_none?(&my_proc) == [1, 2, 3, 4, 5].none?(&my_proc)
+# p [false, nil, false].none? == [false, nil, false].my_none?
+# p %w[dog cat].none?(/x/) == %w[dog cat].my_none?(/x/)
+# p ['dog','car'].none?(5) == ['dog','car'].my_none?(5)
+# p [1, 2, 3].count(&my_proc)
+# p [1, 2, 3].my_none?(1)
+
+# p [1, 2, 3].my_each_with_index.class == Enumerator
+# array =  [1, 2, 3]
+# block = proc { |num| num < 4 }
+# p array.my_each_with_index(&block)
